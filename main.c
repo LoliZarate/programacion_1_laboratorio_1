3f3e0c63d3bbd5fa97eb7977ee2d10ef78163660
LoliZarate <36246933+LoliZarate@users.noreply.github.com>
2018-10-22 12:37:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,37 +3,72 @@
 
 int main()
 {
-    int opcion;
-    float op1=0, op2=0
-    do{
-        printf("Menu");
-        printf("\n\n1. Ingresar 1er operando (A=%.0f)", op1);
-        printf("\n2. Ingresar 2do operando (B=%.0f)", op2);
+    int* vec;
+    int* aux;
 
-        printf("\n\n Seleccione opcion: ");
-        scanf("%d", &opcion);
-    }
-    switch(opcion){
+    vec = (int*) malloc(sizeof(int) * 5);
 
-          case 1:
-               -
-               break;
-          case 2:
-               -
-               break;
-          case 3:
-               -
-               break;
-          case 4:
-               -
-               break;
-          case 5:
-               -
-               break;
-          default:
-               -
+    if(vec == NULL){
+
+        printf("No se consiguio memoria\n");
+        exit(1);
     }
- while (opcion!=5);
-  return 0;
+
+    for(int i=0; i<5; i++){
+
+
+        printf("Ingrese un numero: ");
+        scanf("%d", vec +i);
+    }
+
+    for(int i=0; i<5; i++){
+
+
+        printf("%d ", *(vec + i));
+    }
+
+        printf("\n\n");
+
+
+
+        aux = (int*)realloc(vec, sizeof(int)*10);
+
+        if(aux == NULL){
+            printf("No se pudo agrandar el array\n");
+        }
+        else{
+
+            vec = aux;
+
+            for(int i=5; i<10; i++){
+                 printf("Ingrese un numero: ");
+                scanf("%d", vec +i);
+
+            }
+
+        }
+
+         for(int i=0; i<10; i++){
+
+
+        printf("%d ", *(vec + i));
+    }
+
+        printf("\n\n");
+
+
+    vec  = (int*) realloc(vec, sizeof(int)* 5);
+
+  for(int i=0; i<10; i++){
+
+
+        printf("%d ", *(vec + i));
+    }
+
+        printf("\n\n");
+
+
+        free(vec);
+
+    return 0;
 }
-
